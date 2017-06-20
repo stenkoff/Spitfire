c32bb3d0bcdd0e1604743a9a62460d4c80677d5b
stenkoff <sshenkoff@gmail.com>
2017-06-19 22:56:12 -0400

    create greeting component/container

---
+++ frontend/components/greeting/greeting_container.js
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import Greeting from './greeting';
+
+const mapStateToProps = state => ({
+  currentUser: state.session.currentUser
+});
+
+const mapDispatchToProps = dispatch => ({
+  logout: () => dispatch(logout())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Greeting);
