3f713565f2730c3bccddb13f4a715aeaeb914771
stenkoff <sshenkoff@gmail.com>
2017-06-19 22:18:04 -0400

    Create user actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,30 @@
+import * as APIUtil from '../util/session_api_util'
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveCurrentUser = currentUser => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signup = user => dispatch => {
+  return APIUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      error => dispatch(receiveErrors(error.responseJSON)));
+};
+
+export const login = user => dispatch => {
+  return APIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)),
+      error => dispatch(receiveErrors(error.responseJSON)));
+};
+
+export const logout = () => dispatch => {
+  return APIUtil.logout().then(user => dispatch(receiveCurrentUser(null)))
+};
