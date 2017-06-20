50ecac9769a0da61c5e7d069d4b1820231f14ef4
stenkoff <sshenkoff@gmail.com>
2017-06-19 23:14:47 -0400

    Add frontend auth

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import GreetingContainer from '../greeting/greeting_container';
+import GreetingContainer from './greeting/greeting_container';
+import { Route } from 'react-router-dom';
+import AuthRoute from '../util/route_util';
+
 const App = () => (
   <div>
     <header>
       <h1>Spitfire</h1>
       <GreetingContainer />
     </header>
+    <AuthRoute path="/login" component={SessionFormContainer} />
+    <AuthRoute path="/signup" component={SessionFormContainer} />
   </div>
 );
 
--- frontend/spitfire.jsx
+++ frontend/spitfire.jsx
@@ -3,8 +3,14 @@ import configureStore from './store/store'
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   const root = document.getElementById('root');
-  const store = configureStore();
-  window.store = store;
   ReactDOM.render(<Root store={store}/>, root);
 });
