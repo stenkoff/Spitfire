ea51fa718af6c8b0b58df3707eaeb38138b4d9a4
stenkoff <sshenkoff@gmail.com>
2017-08-21 17:46:32 -0400

    Fix spacing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   attr_reader :password
-	validates :username, :password_digest, :session_token, presence: true
-	validates :username, uniqueness: true
-	validates :password, length: {minimum: 6}, allow_nil: :true
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6}, allow_nil: :true
   after_initialize :ensure_session_token
 
   has_many :playlists
@@ -10,29 +10,29 @@ class User < ActiveRecord::Base
   has_many :albums, through: :tracks;
   has_many :artists, through: :tracks
 
-	def self.find_by_credentials(username, password)
-		user = User.find_by(username: username)
-		user && user.is_password?(password) ? user : nil
-	end
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.is_password?(password) ? user : nil
+  end
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
     @password = password
   end
 
-	def is_password?(password)
-		BCrypt::Password.new(self.password_digest).is_password?(password)
-	end
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 
-	def reset_session_token!
-		self.session_token ||= SecureRandom::urlsafe_base64(16)
-		self.save!
-		self.session_token
-	end
+  def reset_session_token!
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
 
-	private
-	def ensure_session_token
-		self.session_token ||= SecureRandom::urlsafe_base64(16)
-	end
-  
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64(16)
+  end
+
 end
